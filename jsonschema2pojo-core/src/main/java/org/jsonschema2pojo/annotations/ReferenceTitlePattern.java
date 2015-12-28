5b4277e12187c25e6f10415172090614cb584f37
Kathryn Killebrew <kkillebrew@azavea.com>
2015-12-28 12:34:05 -0500

    Annotate reference fields with the target and title pattern

--- jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/annotations/ReferenceTitlePattern.java
+++ jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/annotations/ReferenceTitlePattern.java
@@ -23,6 +23,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface Description {
+public @interface ReferenceTitlePattern {
     String value();
 }
+
